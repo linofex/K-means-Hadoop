aeb817a8ebb75e8429ae9caba6078d541ec583b6
linofex <alessandro.noferi@gmail.com>
2020-06-11 19:17:06 +0200

    Mean class

---
+++ Kmeans/src/Mean.java
@@ -0,0 +1,40 @@
+package Kmeans.src;
+
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.Text;
+import java.io.IOException;
+import java.io.DataInput;
+import java.io.DataOutput;
+
+public class Mean extends Point implements Writable, Comparable<Mean>{
+  private Text id; // Text should save byte wrt IntWritable
+
+  public Mean(Point coordinates, Text id){
+    super(coordinates);
+    this.id.set(id);
+  }
+
+  public Text getId(){
+    return this.id;
+  }
+
+  @Override
+  public void write(DataOutput out) throws IOException {
+  super.write(out);
+  this.id.write(out);
+
+  }
+  
+  @Override
+  public void readFields(DataInput in) throws IOException {
+    super.readFields(in);
+    this.id.readFields(in);
+
+  }
+
+  @Override
+  public int compareTo(Mean that){
+    return (this.id.compareTo(that.getId())); // Text compare works as ASCII
+  }
+
+}
