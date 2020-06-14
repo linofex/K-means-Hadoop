951f9106406e09c9b41579ebc3d999d1bad0c510
linofex <alessandro.noferi@gmail.com>
2020-06-14 19:37:49 +0200

    fisrt implementation of the function that takes the initial means

--- generate_points.py
+++ generate_points.py
@@ -5,6 +5,7 @@ from sklearn.datasets import make_blobs
 from sklearn.cluster import KMeans
 import sys, getopt
 from datetime import datetime
+import random
 
 
 def checkInput(argv):
@@ -47,12 +48,13 @@ def savePoints(fileName, points):
         file.write(" ".join(str(round(i, 3)) for i in point))
         file.write('\n')
 
-def plotPoints(points, centers):
+def plotPoints(points, centers, initialMeans):
   if len(points[0]) == 3:
     ax = plt.axes(projection='3d')
     ax.scatter3D(points[:, 0],points[:, 1],points[:, 2], cmap='viridis');
     ax.scatter3D(centers[:, 0],centers[:, 1],centers[:, 2], cmap='viridis');
-    
+    ax.scatter3D(initialMeans[:, 0],initialMeans[:, 1],initialMeans[:, 2], color='r', marker= '^');
+
   elif len(points[0]) == 2:
     plt.scatter(points[:, 0], points[:, 1], s=50);
   else:
@@ -60,8 +62,16 @@ def plotPoints(points, centers):
     return
   plt.show()
 
-def generateRandomCluster(points):
-  pass
+
+# These centroids shoud be placed in a cunning way because  of different 
+# location causes different result. So, the better choice is to place 
+# them as much as possible far away from each other
+
+def generateRandomMeans(points, clusters):
+  points = random.sample(list(points), clusters)
+  points = [p.tolist() for p in points]
+  return np.array(points)
+
 
 # it performs the K-means alg provided by sklearn.cluster
 def computeDesiredOutput(points, clusters):
@@ -78,31 +88,37 @@ def generateTimestamp():
   # dd/mm/YY H:M:S
   timestamp = now.strftime("%d-%m-%Y_%H:%M:%S")
   return timestamp
+
 if __name__ == "__main__":
   # generate timestamp to link different files
   timestamp = generateTimestamp()
   
   inputPath = './inputs/'
   desiredOutputPath = './des_output/'
+  meansPath= './initial_means/'
   
-  STD = 0.8 # standard deviation between points in a cluster
+  STD = 2 # standard deviation between points in a cluster
 
   clusters, dimension, pointNumber = checkInput(sys.argv[1:])
 
-  InputFileName = 'input_d=%d_n=%d_k=%d.txt' % (dimension, pointNumber, clusters)
-  ClusterFileName = 'cluster_d=%d_k=%d.txt' % (dimension, clusters)
+  InputFileName = '_input_d=%d_n=%d_k=%d.txt' % (dimension, pointNumber, clusters)
+  ClusterFileName = '_cluster_d=%d_k=%d.txt' % (dimension, clusters)
+  MeansFileName = '_initial_means_k=%d.txt' % (clusters)
   
   # dataset genaration
   points = generatePoints(clusters, dimension, pointNumber)
   
-  #initialCluster = generateRandomCluster(points)
-
+  # compute the initial means by taking k random points from the data set
+  initialMeans = generateRandomMeans(points, clusters)
+  
   # compute the desired output for a given dataset
   centers = computeDesiredOutput(points, clusters)
 
   savePoints(inputPath + timestamp + InputFileName, points)
   savePoints(desiredOutputPath + timestamp + ClusterFileName, centers)
-  plotPoints(points, centers)
+  savePoints(meansPath + timestamp + MeansFileName, initialMeans)
+  
+  plotPoints(points, centers, initialMeans)
 
 
 
