a19ce2ab9d44a6c222a653a0561ee5896bdd1935
linofex <alessandro.noferi@gmail.com>
2020-06-26 23:56:00 +0200

    cleaned code

--- Kmeans/src/main/java/it/unipi/hadoop/Kmeans/KmeansCombiner.java
+++ Kmeans/src/main/java/it/unipi/hadoop/Kmeans/KmeansCombiner.java
@@ -8,30 +8,9 @@ public class KmeansCombiner extends Reducer<Mean, Point, Mean , Point> { // per 
 
     @Override
     protected void reduce(Mean key, Iterable<Point> points, Context context) throws IOException, InterruptedException {
-        // devo inizializzare il Point value
-        // System.out.println("Combiner: "+ key.toString());
-        // Iterator<Point> pointsIterator=points.iterator();
-        // Point value= pointsIterator.next();
-        // pointsIterator.forEachRemaining(point -> {value.add(point);
-        //     System.out.println("int: mean: " + key.getId() + " count: " + value.getPointCount() );
-        // });
-        // while(pointsIterator.hasNext()){
-        //     Point p = pointsIterator.next();
-        //     System.out.println("value: " + value.toString());
-        //     System.out.println("p: " + p.toString());
-        //     value.add(p);
-        // }
-        Point result = null;
-        for (Point point : points) {
-            if (result == null) {
-                result = new Point(point);
-            } 
-            else {
-                result.add(point);
-            }
-        }
-
-        System.out.println("mean: " + key.getId() + " count: " + result.getPointCount() );
-        context.write(key, result);
+        Iterator<Point> pointsIterator=points.iterator();
+        Point pointsSum= new Point(pointsIterator.next());
+        pointsIterator.forEachRemaining(point -> {pointsSum.add(point);});    
+        context.write(key, pointsSum);
     }
 }
