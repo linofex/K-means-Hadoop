df46e2755f07023fb6f3aeeb1663171594642791
lossi771 <lossi771@gmail.com>
2020-07-02 12:37:14 +0200

    Combiner code cleaning

--- Kmeans/src/main/java/it/unipi/hadoop/Kmeans/KmeansCombiner.java
+++ Kmeans/src/main/java/it/unipi/hadoop/Kmeans/KmeansCombiner.java
@@ -4,8 +4,7 @@ import org.apache.hadoop.mapreduce.Reducer;
 import java.io.IOException;
 import java.util.Iterator;
 
-public class KmeansCombiner extends Reducer<Mean, Point, Mean , Point> { // per ora utilizzo Point
-
+public class KmeansCombiner extends Reducer<Mean, Point, Mean , Point> { 
     @Override
     protected void reduce(Mean key, Iterable<Point> points, Context context) throws IOException, InterruptedException {
         Iterator<Point> pointsIterator=points.iterator();
