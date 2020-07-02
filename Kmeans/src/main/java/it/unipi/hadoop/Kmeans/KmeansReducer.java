802ff57618ea533a034ed875450e2de11cc0f5f4
lossi771 <lossi771@gmail.com>
2020-07-02 12:37:33 +0200

    Reducer code cleaning

--- Kmeans/src/main/java/it/unipi/hadoop/Kmeans/KmeansReducer.java
+++ Kmeans/src/main/java/it/unipi/hadoop/Kmeans/KmeansReducer.java
@@ -1,18 +1,11 @@
 package it.unipi.hadoop.Kmeans;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.fs.FileSystem;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
 
-import java.util.ArrayList;
 import java.util.Iterator;
 
 public class KmeansReducer extends Reducer<Mean, Point, Text, Text> {
