beac001ed00c1fa353a7c65e8d9f8d9c587b1765
lossi771 <lossi771@gmail.com>
2020-07-02 12:37:55 +0200

    Point code cleaning

--- Kmeans/src/main/java/it/unipi/hadoop/Kmeans/Point.java
+++ Kmeans/src/main/java/it/unipi/hadoop/Kmeans/Point.java
@@ -5,7 +5,6 @@ import java.io.DataOutput;
 import java.io.IOException;
 import org.apache.hadoop.io.Writable;
 
-// import java.util.List;
 
 import org.apache.hadoop.io.ArrayPrimitiveWritable;
 import org.apache.hadoop.io.IntWritable;
